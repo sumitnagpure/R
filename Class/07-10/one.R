a7a6ac287bb8f1bbb921db0ec933cfb2071f01c8
Sumit <sumitnagpure1@gmail.com>
2025-10-07 14:25:05 +0530

    day2

---
+++ Class/07-10/one.R
@@ -0,0 +1,32 @@
+### Control Statements ####
+### If Statement ####
+
+si <- 103
+if (si<200){
+    print("number less than 200")
+}
+
+########## If else Statement ##########
+if (si<100){
+  print("number less than 100")
+} else{
+  print("number more than 100")
+}
+
+########## for loop ##########
+
+for(i in seq(1,10,2)){
+  print(i*i)
+}
+
+
+
+
+
+
+
+
+
+
+
+
